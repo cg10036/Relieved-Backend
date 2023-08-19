c4844a65b56497d04b3e6491423414f289c452bd
cg10036 <cg10036@gmail.com>
2023-08-19 15:57:28 +0900

    first init

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const routes = require("./routes/routes");
+
+app.use(express.json());
+app.use(cors());
+
+app.use(routes);
+
+app.listen(3000, () => console.log("server is listening on port 3000"));
---
+++ routes/routes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const ping = require("./ping");
+
+router.use("/ping", ping);
+
+module.exports = router;
