fc3fcfe5ea7c88f0dca95f449ec85cd923b543f5
cg10036 <cg10036@gmail.com>
2023-08-20 00:47:44 +0900

    code improvement

--- start_or_reload.sh
+++ start_or_reload.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 if [[ `id -u` != 0 ]]; then
-    echo "Must be root to run script"
-    exit
+    echo "restarting as root..."
+    sudo bash start_or_reload.sh
+    exit 0
 fi
 
 md5_ori=($(md5sum start_or_reload.sh))
@@ -13,8 +14,8 @@ if [ "$md5_ori" != "$md5" ]; then
     exit 0
 fi
 
-sudo cp nginx.conf /etc/nginx/nginx.conf
-sudo service nginx restart
+cp nginx.conf /etc/nginx/nginx.conf
+service nginx restart
 npm i
 pm2 delete all
 pm2 start index.js
